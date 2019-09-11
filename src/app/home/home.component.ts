568ed5a8081bf8df2b1c2fd084b9b04d393a182f
Andres Sanabria <andrs.dev@gmail.com>
2019-09-10 20:06:17 -0600

    finished dispersion graphic

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,7 +12,7 @@ export class HomeComponent implements OnInit {
   selectedChart = this.charts[0];
   
   canvasWidth = 1000;
-  canvasHeight = 600;
+  canvasHeight = 400;
 
   plots = [];
 
@@ -93,23 +93,87 @@ export class HomeComponent implements OnInit {
       }
     }
 
-    this.xDistance = this.maxX - this.minX;
-    this.yDistance = this.maxY - this.minY;
-
-
     this.drawCartesianPlane();
+    
+    switch (this.selectedChart) {
+      case 'Dispersion':
+        this.drawDispersionGraphic();
+        break;
+    
+      default:
+        break;
+    }
 
+    
+    
     console.log(this.plots);
   }
 
   drawCartesianPlane(){
-   let element = <HTMLCanvasElement> document.getElementById("canvas");
-   let canvas = element.getContext("2d");
+    let element = <HTMLCanvasElement> document.getElementById("canvas");
+    let canvas = element.getContext("2d");
 
-   
-  
-   canvas.fillStyle = "#00FF00";
-   canvas.fillRect(0, 0, 1000, 600);
+    let maxDistanceX = Math.max(Math.abs(this.minX), Math.abs(this.maxX));
+    let maxDistanceY = Math.max(Math.abs(this.minY), Math.abs(this.maxY));
+
+    canvas.moveTo(this.canvasWidth / 2, 0);
+    canvas.lineTo(this.canvasWidth / 2, this.canvasHeight);
+
+    canvas.moveTo(0, this.canvasHeight / 2);
+    canvas.lineTo(this.canvasWidth, this.canvasHeight / 2);
+
+    canvas.fillText("Y", this.canvasWidth / 2 + 10, 10);
+    canvas.fillText("X", this.canvasWidth - 10, this.canvasHeight / 2 - 12);
+
+    this.xDistance = maxDistanceX / 5;
+    let xDistanceAcum = -maxDistanceX;
+    for (let i = this.canvasWidth / 10; i < this.canvasWidth; i+=this.canvasWidth / 10) {   
+      canvas.moveTo(i, this.canvasHeight / 2 - 5);
+      canvas.lineTo(i, this.canvasHeight / 2 + 5);
+      
+      xDistanceAcum+= this.xDistance;
+      if(i != this.canvasWidth / 2){
+        canvas.fillText(Math.round(xDistanceAcum).toString(), i - 4, this.canvasHeight / 2 + 24);
+      }
+
+    }
+
+    this.yDistance = maxDistanceY / 5;
+    let yDistanceAcum = maxDistanceY;
+    for (let i = this.canvasHeight / 10; i < this.canvasHeight; i+=this.canvasHeight / 10) {   
+      canvas.moveTo(this.canvasWidth / 2 - 5, i);
+      canvas.lineTo(this.canvasWidth / 2 + 5, i);
+      
+      yDistanceAcum-= this.yDistance;
+      if(i != this.canvasHeight / 2){
+        canvas.fillText(Math.round(yDistanceAcum).toString(), this.canvasWidth / 2 - 24, i + 2);
+      }
+    }
+
+    canvas.stroke();
+  }
+
+  drawDispersionGraphic(){
+    let element = <HTMLCanvasElement> document.getElementById("canvas");
+    let canvas = element.getContext("2d");
+
+    let xProportion = (this.canvasWidth / 2) / this.maxX;
+    let yProportion = (this.canvasHeight / 2) / this.maxY;
+
+    canvas.fillStyle = "#0000FF";
+    canvas.moveTo(this.canvasWidth / 2, this.canvasHeight / 2);
+    for (let i = 0; i < this.plots.length; i++) {
+
+      const plot: any = this.plots[i];
+      let x = this.canvasWidth / 2 + plot.x*xProportion;
+      let y = this.canvasHeight / 2 - plot.y*yProportion;
+
+
+      canvas.fillRect(x - 2, y - 1.5 , 4, 3);
+      
+    }
+
+    canvas.stroke();
   }
 
 
